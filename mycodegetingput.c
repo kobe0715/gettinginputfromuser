84b9dc6a23cf022c6f6808ff9f241c52fbcd3905
kobe0715 <123227244+kobe0715@users.noreply.github.com>
2023-01-21 17:38:25 +0800

    Create mycodegetingput.c
    it allow user to enter grade name and age

---
+++ mycodegetingput.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main(void){
+char name[50[;
+int age;
+int grade;
+
+printf("please enter youre name\n:");
+gets(name);
+printf("hello you are %s:\n");
+
+printf("please enter youre age\n:");
+gets(age);
+printf("hello you are %d yrs old:\n");
+
+printf("whats youre grade now?\n:");
+gets(grade);
+printf("hello you are %d:\n");
+
+
+
+
+
+
+
+
+
+
+
+}
